d3b02b6d7ec0af8f4e354b2c71bb07271051c1b1
Ania Gonzalez <aniagr@gmail.com>
2016-07-01 13:35:13 -0700

    added changin colors with diff player turns

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -21,24 +21,55 @@
 // RENDER function that can show what the GAMEBOARD looks like
 
 // GAME OVER when no more spots are available
-function TicTacToe() {
-  this.board = [[0,0,0], [0,0,0], [0,0,0]]
+function TicTacToe(player1, player2) {
+  this._board = [[0,0,0], [0,0,0], [0,0,0]]
+  this._player1 = player1 //even turn player => 1
+  this._player2 = player2 //odd turn player => -1
+  this._turn = 0
 
 }
 
-TicTacToe.prototype.play = function(selectedCol, selectedRow) {
-  //
-  console.log(this.board[selectedRow][selectedCol])
-}
+TicTacToe.prototype = {
 
-function Player() {
+  checkAvailable: function(currentPosition){
+    return currentPosition === 0
+  },
+
+  play: function(selectedRow, selectedCol) {
+
+    var currentPosition = this._board[selectedRow][selectedCol]
+
+    console.log(currentPosition)
+
+    if(this.checkAvailable(currentPosition)){
+      if(this._turn % 2 == 0) {
+        this._board[selectedRow][selectedCol] = 1
+        var playerClass = "player1"
+      } else {
+        this._board[selectedRow][selectedCol] = -1
+        var playerClass = "player2"
+      }
+      console.log("its yours!", currentPosition)
+      console.log(this._board)
+
+
+      this._turn ++
+      return playerClass
+    } else {
+      console.log("already taken, try again!")
+    }
+  },
+
+  checkWin: function() {
+    
+  }
 
 }
 
 
 $(document).on('ready', function() {
 
-  var game = new TicTacToe
+  var game = new TicTacToe("p1", "p2")
 
   var buttons = $(".tic-tac-toe button")
 
@@ -48,7 +79,14 @@ $(document).on('ready', function() {
     var selectedRow = $(this).data('row')
     console.log("col:", selectedCol)
     console.log("row:", selectedRow)
-    game.play(selectedCol, selectedRow)
+
+    var colorClass = game.play(selectedRow, selectedCol)
+
+    $(this).addClass(colorClass)
+
+    //check if win?
+    game.checkWin
+
   })
 
 })
